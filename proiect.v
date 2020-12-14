c89435a2eb56469e4adabfed3e3fdd9c274cde6f
alee1900 <59049194+alee1900@users.noreply.github.com>
2020-12-14 16:55:38 +0200

    Add files via upload

---
+++ proiect.v
@@ -0,0 +1,448 @@
+(* Since the variable names are now strings, we need to import the required libraries *)
+Require Import Strings.String.
+Local Open Scope string_scope. 
+Local Open Scope list_scope.
+Scheme Equality for string.
+
+(* ErrorNat encapsulates the constructor error_nat,
+   which is useful in the case of arithmetic operations like division by 0*)
+Inductive ErrorNat :=
+  | error_nat : ErrorNat
+  | num : nat -> ErrorNat.
+
+Inductive ErrorBool :=
+  | error_bool : ErrorBool
+  | boolean : bool -> ErrorBool.
+
+Inductive ErrorStr :=
+  | error_str : ErrorStr
+  | str : string -> ErrorStr.
+
+Coercion num: nat >-> ErrorNat.
+Coercion boolean: bool >-> ErrorBool.
+Coercion str: string >-> ErrorStr.
+
+(* A general type which includes all kind of types *)
+Inductive Result :=
+  | err_undecl : Result
+  | err_assign : Result
+  | default : Result
+  | res_nat : ErrorNat -> Result
+  | res_bool : ErrorBool -> Result
+  | res_str : ErrorStr -> Result.
+
+Scheme Equality for Result.
+
+(* An environment which maps variable names (strings) to the Result type *)
+Definition Env := string -> Result.
+
+(* Initial environment *)
+Definition env : Env := fun x => err_undecl.
+
+(* Initially each variable is undeclared *)
+Compute (env "x").
+
+(* The initial environment "env" maps each variable to an "err_undecl" value, 
+   which means that the variables were not yet declared.
+   In order to update the value of a specific variable, we need to implement
+   an "update" function, which changes the value of a specific variable
+   based on its current value.
+
+  Possible usecases:
+    ~> if the variable is mapped to an "err_undecl" value and we want to update its value
+       with a numerical/boolean value, this is not possible, because the variable was not
+       even declared, and the "update" function returns "err_undecl"
+    ~> if the variable is mapped to a numerical value ("res_nat" constructor), 
+       basically its declaration type is nat, then we can not change its value 
+       to a boolean value and the "update" function returns "err_assign" 
+
+Please note that the previous possible usecases can be different for your implementation... :)
+This is just an idea of how we can simulate the variables declaration + basic typing rules.
+
+Provide your own implementation for the update function.
+
+In order to compare the types of the variables (this can be useful when implementing the "update" function), 
+you can implement a method, "check_eq_over_types", with the following signature:
+
+Definition check_eq_over_types (t1 : Result) (t2 : Result) : bool
+
+*)
+
+Definition check_eq_over_types (t1 : Result) (t2 : Result) : bool :=
+  match t1 with
+    | err_assign => match t2 with 
+                     | err_assign => true
+                     | _ => false
+                     end
+  (* Fill in the implementation for the rest of the cases... *)
+  end.
+
+Definition update (env : Env) (x : string) (v : Result) : Env :=
+  fun y =>
+    if (eqb y x)
+    then
+        (* Fill in with your own implementation... 
+           Handle here each possible usecase when updating the value of a variable.
+        *)
+    else (env y).
+
+Notation "S [ V /' X ]" := (update S X V) (at level 0).
+
+(* Please note that you need to adapt the code below to your own implementation... :) *)
+
+Compute (env "y").
+Compute (update (update env "y" (default)) "y" (res_bool true) "y").
+Compute ((update (update (update env "y" default) "y" (res_nat 10)) "y" (res_bool true)) "y").
+
+
+Inductive AExp :=
+| avar: string -> AExp (* Var ~> string *)
+| anum: ErrorNat -> AExp
+| aplus: AExp -> AExp -> AExp
+| asub: AExp -> AExp -> AExp
+| amul: AExp -> AExp -> AExp (* Multiplication *)
+| adiv: AExp -> AExp -> AExp (* Division *)
+| amod: AExp -> AExp -> AExp. (* Modulo *)
+
+(* Coercions for numerical constants and variables *)
+Coercion anum: ErrorNat >-> AExp.
+Coercion avar: string >-> AExp. (* Var ~> string *)
+
+(* Notations used for arithmetic operations *)
+Notation "A +' B" := (aplus A B)(at level 50, left associativity).
+Notation "A -' B" := (asub A B)(at level 50, left associativity).
+Notation "A *' B" := (amul A B)(at level 48, left associativity).
+Notation "A /' B" := (adiv A B)(at level 48, left associativity).
+Notation "A %' B" := (amod A B)(at level 45, left associativity).
+
+(* Notatations used for the Big-Step semantics *)
+Reserved Notation "A =[ S ]=> N" (at level 60).
+Reserved Notation "B ={ S }=> B'" (at level 70).
+
+Definition plus_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 + v2)
+    end.
+
+Definition sub_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num n1, num n2 => if Nat.ltb n1 n2
+                        then error_nat
+                        else num (n1 - n2)
+    end.
+
+Definition mul_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 * v2)
+    end.
+
+Definition div_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (Nat.div v1 v2)
+    end.
+
+Definition mod_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (v1 - v2 * (Nat.div v1 v2))
+    end.
+
+Fixpoint aeval_fun (a : AExp) (env : Env) : ErrorNat :=
+  match a with
+  | avar v => match (env v) with
+                | res_nat n => n
+                | _ => error_nat
+                end
+  | anum v => v
+  | aplus a1 a2 => (plus_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | amul a1 a2 => (mul_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | asub a1 a2 => (sub_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | adiv a1 a2 => (div_ErrorNat  (aeval_fun a1 env) (aeval_fun a2 env))
+  | amod a1 a2 => (mod_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  end.
+
+(* Big-Step semantics for arithmetic operations *)
+Inductive aeval : AExp -> Env -> ErrorNat -> Prop :=
+| const : forall n sigma, anum n =[ sigma ]=> n
+| var : forall v sigma, avar v =[ sigma ]=>  match (sigma v) with
+                                              | res_nat x => x
+                                              | _ => error_nat
+                                              end
+| add : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (plus_ErrorNat i1 i2) ->
+    a1 +' a2 =[sigma]=> n
+| times : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (mul_ErrorNat i1 i2) ->
+    a1 *' a2 =[sigma]=> n
+| substract : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (sub_ErrorNat i1 i2) ->
+    a1 -' a2 =[sigma]=> n
+| division : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (div_ErrorNat  i1 i2) ->
+    a1 /' a2 =[sigma]=> n
+| modulo : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = (mod_ErrorNat i1 i2) ->
+    a1 %' a2 =[sigma]=> n
+where "a =[ sigma ]=> n" := (aeval a sigma n).
+
+Example substract_error : 1 -' 5 =[ env ]=> error_nat.
+Proof.
+  eapply substract.
+  - apply const.
+  - apply const.
+  - simpl. reflexivity.
+Qed.
+
+Example division_error : 3 /' 0 =[ env ]=> error_nat.
+Proof.
+  eapply division.
+  - apply const.
+  - apply const.
+  - simpl. reflexivity.
+Qed.
+
+Example modulo_error : 3 %' 0 =[ env ]=> error_nat.
+Proof.
+  eapply modulo.
+  - apply const.
+  - apply const.
+  - simpl. reflexivity.
+Qed.
+
+Inductive BExp :=
+| berror
+| btrue
+| bfalse
+| bvar: string -> BExp (* Variables of type bool *)
+| blt : AExp -> AExp -> BExp
+| bnot : BExp -> BExp
+| band : BExp -> BExp -> BExp
+| bor : BExp -> BExp -> BExp.
+
+(* Notations used for boolean operations *)
+Notation "A <' B" := (blt A B) (at level 70).
+Notation "!' A" := (bnot A)(at level 51, left associativity).
+Notation "A &&' B" := (band A B)(at level 52, left associativity).
+Notation "A ||' B" := (bor A B)(at level 53, left associativity).
+
+Definition lt_ErrorBool (n1 n2 : ErrorNat) : ErrorBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.ltb v1 v2)
+    end.
+
+Definition not_ErrorBool (n :ErrorBool) : ErrorBool :=
+  match n with
+    | error_bool => error_bool
+    | boolean v => boolean (negb v)
+    end.
+
+Definition and_ErrorBool (n1 n2 : ErrorBool) : ErrorBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (andb v1 v2)
+    end.
+
+Definition or_ErrorBool (n1 n2 : ErrorBool) : ErrorBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (orb v1 v2)
+    end.
+
+Fixpoint beval_fun (a : BExp) (envnat : Env) : ErrorBool :=
+  match a with
+  | btrue => true
+  | bfalse => false
+  | berror => error_bool
+  | bvar v => match (env v) with
+                | res_bool n => n
+                | _ => error_bool
+                end
+  | blt a1 a2 => (lt_ErrorBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bnot b1 => (not_ErrorBool (beval_fun b1 envnat))
+  | band b1 b2 => (and_ErrorBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  | bor b1 b2 => (or_ErrorBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  end.
+
+Reserved Notation "B ={ S }=> B'" (at level 70).
+Inductive beval : BExp -> Env -> ErrorBool -> Prop :=
+| b_error: forall sigma, berror  ={ sigma }=> error_bool
+| b_true : forall sigma, btrue ={ sigma }=> true
+| b_false : forall sigma, bfalse ={ sigma }=> false
+| b_var : forall v sigma, bvar v ={ sigma }=>  match (sigma v) with
+                                                | res_bool x => x
+                                                | _ => error_bool
+                                                end
+| b_lessthan : forall a1 a2 i1 i2 sigma b,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    b = (lt_ErrorBool i1 i2) ->
+    a1 <' a2 ={ sigma }=> b
+| b_not : forall a1 i1 sigma b,
+    a1 ={ sigma }=> i1 ->
+    b = (not_ErrorBool i1) ->
+    !'a1 ={ sigma }=> b
+| b_and : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (and_ErrorBool i1 i2) ->
+    (a1 &&' a2) ={ sigma }=> b 
+| b_or : forall a1 a2 i1 i2 sigma b,
+    a1 ={ sigma }=> i1 ->
+    a2 ={ sigma }=> i2 ->
+    b = (or_ErrorBool i1 i2) ->
+    (a1 ||' a2) ={ sigma }=> b 
+where "B ={ S }=> B'" := (beval B S B').
+
+(* Because "n" is not declared *)
+Example boolean_operation : bnot (100 <' "n") ={ env }=> error_bool.
+Proof.
+  eapply b_not.
+  eapply b_lessthan.
+  - eapply const.
+  - eapply var.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+(* Statements *)
+Inductive Stmt :=
+  | nat_decl: string -> AExp -> Stmt (* Declaration Stmt for variables of type nat *)
+  | bool_decl: string -> BExp -> Stmt (* Declaration Stmt for variables of type bool *)
+  | nat_assign : string -> AExp -> Stmt (* Assignment Stmt for variables of type nat *)
+  | bool_assign : string -> BExp -> Stmt (* Assignment Stmt for variables of type nat *)
+  | sequence : Stmt -> Stmt -> Stmt
+  | while : BExp -> Stmt -> Stmt
+  | ifthenelse : BExp -> Stmt -> Stmt -> Stmt
+  | ifthen : BExp -> Stmt -> Stmt.
+
+Notation "X :n= A" := (nat_assign X A)(at level 90).
+Notation "X :b= A" := (bool_assign X A)(at level 90).
+Notation "'iNat' X ::= A" := (nat_decl X A)(at level 90).
+Notation "'iBool' X ::= A" := (bool_decl X A)(at level 90).
+Notation "S1 ;; S2" := (sequence S1 S2) (at level 93, right associativity).
+Notation "'fors' ( A ~ B ~ C ) { S }" := (A ;; while B ( S ;; C )) (at level 97).
+
+Reserved Notation "S -{ Sigma }-> Sigma'" (at level 60).
+
+Inductive eval : Stmt -> Env -> Env -> Prop :=
+| e_nat_decl: forall a i x sigma sigma',
+   a =[ sigma ]=> i ->
+   sigma' = (update sigma x (res_nat i)) ->
+   (x :n= a) -{ sigma }-> sigma'
+| e_nat_assign: forall a i x sigma sigma',
+    a =[ sigma ]=> i ->
+    sigma' = (update sigma x (res_nat i)) ->
+    (x :n= a) -{ sigma }-> sigma'
+| e_bool_decl: forall a i x sigma sigma',
+   a ={ sigma }=> i ->
+   sigma' = (update sigma x (res_bool i)) ->
+   (x :b= a) -{ sigma }-> sigma'
+| e_bool_assign: forall a i x sigma sigma',
+    a ={ sigma }=> i ->
+    sigma' = (update sigma x (res_bool i)) ->
+    (x :b= a) -{ sigma }-> sigma'
+| e_seq : forall s1 s2 sigma sigma1 sigma2,
+    s1 -{ sigma }-> sigma1 ->
+    s2 -{ sigma1 }-> sigma2 ->
+    (s1 ;; s2) -{ sigma }-> sigma2
+| e_if_then : forall b s sigma,
+    ifthen b s -{ sigma }-> sigma
+| e_if_then_elsetrue : forall b s1 s2 sigma sigma',
+    b ={ sigma }=> true ->
+    s1 -{ sigma }-> sigma' ->
+    ifthenelse b s1 s2 -{ sigma }-> sigma' 
+| e_if_then_elsefalse : forall b s1 s2 sigma sigma',
+    b ={ sigma }=> false ->
+    s2 -{ sigma }-> sigma' ->
+    ifthenelse b s1 s2 -{ sigma }-> sigma' 
+| e_whilefalse : forall b s sigma,
+    b ={ sigma }=> false ->
+    while b s -{ sigma }-> sigma
+| e_whiletrue : forall b s sigma sigma',
+    b ={ sigma }=> true ->
+    (s ;; while b s) -{ sigma }-> sigma' ->
+    while b s -{ sigma }-> sigma'
+where "s -{ sigma }-> sigma'" := (eval s sigma sigma').
+
+Fixpoint eval_fun (s : Stmt) (env : Env) (gas: nat) : Env :=
+    match gas with
+    | 0 => env
+    | S gas' => match s with
+                | sequence S1 S2 => eval_fun S2 (eval_fun S1 env gas') gas'
+                | nat_decl a aexp => update (update env a default) a (res_nat (aeval_fun aexp env))
+                | bool_decl b bexp => update (update env b default) b (res_bool (beval_fun bexp env))
+                | nat_assign a aexp => update env a (res_nat (aeval_fun aexp env))
+                | bool_assign b bexp => update env b (res_bool (beval_fun bexp env))
+                | ifthen cond s' => 
+                    match (beval_fun cond env) with
+                    | error_bool => env
+                    | boolean v => match v with
+                                 | true => eval_fun s' env gas'
+                                 | false => env
+                                 end
+                    end
+                | ifthenelse cond S1 S2 => 
+                    match (beval_fun cond env) with
+                        | error_bool => env
+                        | boolean v  => match v with
+                                 | true => eval_fun S1 env gas'
+                                 | false => eval_fun S2 env gas'
+                                 end
+                         end
+                | while cond s' => 
+                    match (beval_fun cond env) with
+                        | error_bool => env
+                        | boolean v => match v with
+                                     | true => eval_fun (s' ;; (while cond s')) env gas'
+                                     | false => env
+                                     end
+                        end
+                end
+    end.
+
+Definition while_stmt :=
+    iNat "i" ::= 0 ;;
+    iNat "sum" ::= 0 ;;
+    while 
+        ("i" <' 6) 
+        (
+           "sum" :n= "sum" +' "i" ;;
+           "i" :n= "i" +' 1
+        ).
+
+Compute (eval_fun while_stmt env 100) "sum".
+
+Definition for_stmt :=
+    iNat "sum" ::= 0 ;;
+    fors ( iNat "i" ::= 0 ~ "i" <' 6 ~ "i" :n= "i" +' 1 ) {
+      "sum" :n= "sum" +' "i"
+    }.
+
+
+Compute (eval_fun for_stmt env 100) "sum".
+
